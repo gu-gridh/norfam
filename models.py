be0dc8792455f06ce7d33f0cf2ce388bbfb11ca4
AramKarimi <aram.karimi@gu.se>
2022-11-01 13:40:34 +0100

    update names

--- models.py
+++ models.py
@@ -18,59 +18,59 @@ class Term(abstract.AbstractBaseModel):
 
     term_id = models.AutoField(primary_key=True, verbose_name= _("id"))
     # The word in lowercase
-    term_term = models.CharField(max_length=100, verbose_name= _("term_term"))
+    term_term = models.CharField(max_length=100, verbose_name= _("term"))
     # # Not used?
-    term_stem = models.CharField(max_length=100, verbose_name= _("term_stem"), blank=True, null=True)
+    term_stem = models.CharField(max_length=100, verbose_name= _("term stem"), blank=True, null=True)
     # # Document frequency, how many documents contain the word at least once
-    term_df = models.IntegerField(verbose_name= _("term_document_frequency"), blank=True, null=True)
+    term_df = models.IntegerField(verbose_name= _("document frequency of term"), blank=True, null=True)
     # # Documents edition
     version = models.IntegerField(verbose_name= _("version"), blank=True, null=True)
 
     class Meta:
         # managed = False
         unique_together = [['term_term', 'version']]
-        verbose_name = _("norfam.term")
-        verbose_name_plural = _("norfam.term.plural")
+        verbose_name = _("term")
+        verbose_name_plural = _("terms")
 
 
 class DocTerm(abstract.AbstractBaseModel):
     """Mapping of a certain term in a certain document."""
 
-    doc_term_id = models.AutoField(primary_key=True, verbose_name= _("id"))
+    doc_term_id = models.AutoField(primary_key=True, verbose_name= _("ID"))
     # The document where it occurs at least once
-    doc_id = models.ForeignKey('Document', on_delete=models.CASCADE, db_column='doc_id', related_name='doc_terms', verbose_name= _("id"))
+    doc_id = models.ForeignKey('Document', on_delete=models.CASCADE, db_column='doc_id', related_name='doc_terms', verbose_name= _("document ID"))
     # The term
     term = models.ForeignKey('Term', on_delete=models.CASCADE, db_column='term_id', verbose_name= _("term"))
     # Term frequency, how many times the term occurs in the document
-    tf = models.IntegerField(verbose_name= _("term_frequency"), blank=True, null=True)
+    tf = models.IntegerField(verbose_name= _("term frequency"), blank=True, null=True)
     # # Documents edition
     version = models.IntegerField(verbose_name= _("version"), blank=True, null=True)
 
     class Meta:
         # managed = False
-        verbose_name = _("norfam.document_term")
-        verbose_name_plural = _("norfam.document_term.plural")
+        verbose_name = _("document-term relation")
+        verbose_name_plural = _("document-term relations")
 
 
 class Document(abstract.AbstractBaseModel):
     """An encyclopedic article on a topic."""
 
-    doc_id = models.AutoField(primary_key=True, verbose_name= _("id"))
+    doc_id = models.AutoField(primary_key=True, verbose_name= _("ID"))
     # The topic, one or a few words
-    doc_keyword = models.CharField(max_length=100, verbose_name= _("document_keyword"), blank=True, null=True)
+    doc_keyword = models.CharField(max_length=100, verbose_name= _("document keyword"), blank=True, null=True)
     # Full description, may contain HTML tags
-    doc_text = models.TextField(verbose_name= _("document_text"), blank=True, null=True)
+    doc_text = models.TextField(verbose_name= _("text"), blank=True, null=True)
     # Truncated description, max ca. 700 chars, possibly no incomplete HTML tags
-    doc_abstr = models.CharField(max_length=2048, verbose_name= _("document_abstract"), blank=True, null=True)
+    doc_abstr = models.CharField(max_length=2048, verbose_name= _("abstract"), blank=True, null=True)
     # Running number unique among documents with same keyword, starting from 0
-    doc_suppl = models.IntegerField(verbose_name= _("document_suppl"), blank=True, null=True)
+    doc_suppl = models.IntegerField(verbose_name= _("running document ID"), blank=True, null=True)
     # # Documents edition
     version = models.IntegerField(verbose_name= _("version"), blank=True, null=True)
 
     class Meta:
         # managed = False
-        verbose_name = _("norfam.document")
-        verbose_name_plural = _("norfam.document.plural")
+        verbose_name = _("document")
+        verbose_name_plural = _("documents")
 
 
 class Termsim(abstract.AbstractBaseModel):
@@ -78,7 +78,7 @@ class Termsim(abstract.AbstractBaseModel):
 
     Each term pair occurs twice – once in each direction."""
 
-    termsim_id = models.AutoField(primary_key=True, verbose_name= _("id"))
+    termsim_id = models.AutoField(primary_key=True, verbose_name= _("ID"))
     # Term one
     target = models.ForeignKey('Term', on_delete=models.CASCADE, db_column='term1_id', related_name='neighbors', verbose_name= _("target"))
     # Term two
@@ -89,18 +89,18 @@ class Termsim(abstract.AbstractBaseModel):
 
     class Meta:
         # managed = False
-        verbose_name = _("norfam.term_similarity")
-        verbose_name_plural = _("norfam.term_similarity.plural")
+        verbose_name = _("term similarity")
+        verbose_name_plural = _("term similarities")
 
 
 class Entity(abstract.AbstractBaseModel):
     ent_id = models.AutoField(primary_key=True, verbose_name= _("id"))
-    doc_id =  models.ForeignKey('Document', on_delete=models.CASCADE, db_column='doc_id')
-    ent_type = models.TextField(verbose_name= _("entity_type"), blank=True, null=True)
-    ent_name = models.TextField(verbose_name= _("entity_name"), blank=True, null=True)
+    doc_id =  models.ForeignKey('Document', on_delete=models.CASCADE, db_column='doc_id', verbose_name=_("document"))
+    ent_type = models.TextField(verbose_name= _("entity type"), blank=True, null=True)
+    ent_name = models.TextField(verbose_name= _("entity name"), blank=True, null=True)
     version = models.IntegerField(verbose_name= _("version"), blank=True, null=True)
 
     class Meta:
         # managed = False
-        verbose_name = _("norfam.entity")
-        verbose_name_plural = _("norfam.entity.plural")
+        verbose_name = _("entity")
+        verbose_name_plural = _("entities")
