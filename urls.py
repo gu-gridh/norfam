a7f397fdcf622fb3057a870b413e631573e20b3c
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-11-28 14:53:23 +0100

    Updated api urls

--- urls.py
+++ urls.py
@@ -1,15 +1,29 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+import diana.utils as utils
+
 
 router = routers.DefaultRouter()
-router.register(r'api/terms', views.TermViewSet, basename="term")
-router.register(r'api/termsim', views.TermsimViewSet, basename="termsim")
-router.register(r'api/entities', views.EntityViewSet, basename="entity")
-router.register(r'api/documents', views.DocumentViewSet, basename="document")
-router.register(r'api/query', views.QueryViewSet, basename="api")
+endpoint = utils.build_app_endpoint("norfam")
+documentation = utils.build_app_api_documentation("norfam", endpoint)
+
+
+router = routers.DefaultRouter()
+
+router.register(rf'{endpoint}/terms', views.TermViewSet, basename="term")
+router.register(rf'{endpoint}/termsim', views.TermsimViewSet, basename="termsim")
+router.register(rf'{endpoint}/entities', views.EntityViewSet, basename="entity")
+router.register(rf'{endpoint}/documents', views.DocumentViewSet, basename="document")
+router.register(rf'{endpoint}/query', views.QueryViewSet, basename="api")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+    *documentation,
+
+    # Automatically generated views
+    *utils.get_model_urls('norfam', f'{endpoint}', exclude=['page', 'work', 'work_authors']),
+    path(f'{endpoint}/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
